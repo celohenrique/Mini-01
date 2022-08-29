dff25ce0cc77e50b7a1f4a5adcdada6692a89b73
Marcelo Henrique <celohenriquech7@gmail.com>
2022-08-29 15:19:01 -0300

    Initial Commit

---
+++ Mini#01/Mini_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini_01App.swift
+//  Mini#01
+//
+//  Created by Marcelo Araujo on 29/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Mini_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

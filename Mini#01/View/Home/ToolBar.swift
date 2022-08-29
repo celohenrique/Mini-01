6d6ba677026daf52d5590e61e8179edbfd28bfcc
Marcelo Henrique <celohenriquech7@gmail.com>
2022-08-29 17:47:20 -0300

    ToolBar
    Created ToolBar

---
+++ Mini#01/View/Home/ToolBar.swift
@@ -0,0 +1,20 @@
+//
+//  ToolBar.swift
+//  Mini#01
+//
+//  Created by Marcelo Araujo on 29/08/22.
+//
+
+import SwiftUI
+
+struct ToolBar: View {
+    var body: some View {
+        NavigationView{
+            VStack {
+                
+            }
+            .navigationTitle("Sounds")
+        }
+    }
+}
+

64ed10aef203de8fd16d92f905cb0a3fb1adcd11
Marcelo Henrique <celohenriquech7@gmail.com>
2022-09-14 15:54:11 -0300

    first try

---
+++ Mini#01/View/SegundaTela/PickerScreen.swift
@@ -0,0 +1,20 @@
+//
+//  PickerScreen.swift
+//  Mini#01
+//
+//  Created by Marcelo Araujo on 14/09/22.
+//
+
+import SwiftUI
+
+struct PickerScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PickerScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerScreen()
+    }
+}

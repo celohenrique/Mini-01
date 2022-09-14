2f14f04e3af54bb6669854689008bb2f3c17aebf
Marcelo Henrique <celohenriquech7@gmail.com>
2022-09-14 16:13:48 -0300

    final arrumacao

--- Mini#01/View/SegundaTela/SegundaTela.swift
+++ Mini#01/View/SegundaTela/SegundaTela.swift
@@ -7,113 +7,31 @@
 
 import SwiftUI
 
-struct CircleButton: ButtonStyle{
-    
-    internal func makeBody(configuration: ButtonStyleConfiguration) -> some View {
-        Circle()
-            .fill()
-            .overlay(
-                Circle()
-                    .fill(Color.white)
-                    .opacity(configuration.isPressed ? 0.3 : 0) //use : for otherwise, so opacity will be 0.3 otherwise will be 0
-            )
-            .overlay(
-                Circle()
-                    .stroke(lineWidth: 2)
-                    .foregroundColor(.white)
-                    .padding(4))
-            .overlay(
-                configuration.label
-                    .foregroundColor(.black))
-    }
-}
+
 
 struct SegundaTela: View {
     
     @State var ruidos: Ruidos
-    
-    @State var hourSelection = 0
-    @State var minuteSelection = 0
-    
-   @State var timerOnOff = true //usei para mudar do picker para o countdown
-    @State var isPlaying = true
-
-    var hours = [Int](0..<24)
-    var minutes = [Int](0..<60)
-    
-    var body: some View {
-
-            GeometryReader{ geometry in
-                VStack(alignment: .center,spacing: 100){
-                    HStack(alignment: .center){
-                        
-                        Picker(selection: self.$hourSelection, label: Text("")){
-                            ForEach(0 ..< self.hours.count){ index in
-                                Text("\(self.hours[index]) hours").tag(index)
-                            }
-                        }
-                        .pickerStyle(WheelPickerStyle())
-                        .frame(width: geometry.size.width/2, height: 150, alignment: .center)
-                        // .compositingGroup()
-                        // .clipped()
-                
-                        Picker(selection: self.$minuteSelection, label: Text("")){
-                            ForEach(0 ..< self.minutes.count){ index in
-                                Text("\(self.minutes[index]) min").tag(index)
-                            }
-                        }
-                        .pickerStyle(WheelPickerStyle())
-                        .frame(width: geometry.size.width/2, height: 150, alignment: .center)
-                        // .compositingGroup()
-                        //.clipped()
-    
-                    }
-                    HStack(spacing:170){
-                        
-                        Button(action: {}){ //action here
-                            
-                            Text("Cancel")
-                        }
-                        
-                        .frame(width: 100, height: 100)
-                        .foregroundColor(Color(red: 228/255, green: 228/255, blue: 228/255))
-                        // .frame(width: 75, height: 75)
-                        
-                        Button(action:{
-                            
-                            timerOnOff = false //Start button action
-                            
-                        }){
-                            Text("Start")
-                                .foregroundColor(Color(red: 61/255, green: 197/255, blue: 94/255))
-                        }
-                        .frame(width: 100, height: 100)
-                        .foregroundColor(Color(red: 105/255, green: 152/255, blue: 117/255))
-                        // .frame(width: 75, height: 75)
-                    }
-                    .buttonStyle(CircleButton())
-                }
-                VStack {
-                    Text(ruidos.nome)
-                        .font(.title)
-                    Spacer()
-                        .frame(height: 500)
-                        
-                }
-            }
-        }
+    var body: some View{
+    ZStack{
+        
+        //colocar background
+        
+        VStack {
+            Text(ruidos.nome)
+                .font(.title)
+            Spacer()
+                .frame(height: 500)
             
         }
-
-extension UIPickerView {
-    open override var intrinsicContentSize: CGSize {
-        return CGSize(width: UIView.noIntrinsicMetric, height: super.intrinsicContentSize.height)
-        
+    }
     }
 }
 
-        struct SegundaTela_Previews: PreviewProvider {
-            static var previews: some View {
-                SegundaTela()
-            }
-        }
+
+
+//        struct SegundaTela_Previews: PreviewProvider {
+//            static var previews: some View {
+//                SegundaTela()
+//            }
+//        }

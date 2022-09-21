7eb2e3e78886f33cefb9510a9fd069a9bbce776a
Sarah <91162156+SarahSilva0@users.noreply.github.com>
2022-09-21 16:57:27 -0300

    arrumando o espacamento da half sheet

--- Mini#01/View/Home/TelaInicial.swift
+++ Mini#01/View/Home/TelaInicial.swift
@@ -88,7 +88,7 @@ struct TelaInicial: View {
                             .frame(height: 20)
                         Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.")
                         Spacer()
-                            .frame(height: 100)
+                            .frame(height: 90)
                     }
                     
                 }.height(.proportional(0.55))
